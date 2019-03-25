3ba4a939b8473e54156ad6e742274c704955623d
GBTC440010UR <GBTC440010UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-25 19:13:31 -0400

    Initial commit

---
+++ src/main/java/com/sammy/springboot301_1/Springboot3011Application.java
@@ -0,0 +1,13 @@
+package com.sammy.springboot301_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3011Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3011Application.class, args);
+	}
+
+}
